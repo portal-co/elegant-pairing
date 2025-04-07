ed40a5ec741b0926bbdf7c2d079cdaffe309817a
Graham Kelly <gkgoat6700@gmail.com>
2025-04-07 20:20:45 +0000

    init

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+use num_integer::Roots;
+
+pub fn pair<I: Roots + Clone>(n: I, m: I) -> I{
+    if n >= m{
+        return n.clone() * n.clone() + n + m;
+    }
+    return m.clone() * m + n;
+}
+pub fn unpair<I: Roots + Clone>(z: I) -> (I,I){
+    let q = z.sqrt();
+    let l = z - q.clone() * q.clone();
+    if l < q{
+        return (l,q);
+    }
+    return (q.clone(),l - q);
+}
